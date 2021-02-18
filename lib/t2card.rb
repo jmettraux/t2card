66d2ba231627e4f89927a59fd3c0cd591e713513
John Mettraux <jmettraux@gmail.com>
2021-02-18 16:27:03 +0900

    Print --ruler

--- lib/t2card.rb
+++ lib/t2card.rb
@@ -1,13 +1,91 @@
 
+args = { fnames: [] }
+  key = nil
+  ARGV.each do |a|
+    case a
+    when /^-{1,2}(.+)$/
+      args[key] = true if key
+      key = $1
+    else
+      if key == nil
+        args[:fnames] << a
+      else
+        args[key] = a
+        key = nil
+      end
+    end
+  end
+  args[key] = true if key
+
+if args['h'] || args['help']
+  puts
+  puts "Usage: t2card [OPTIONS] [FILE]"
+  puts
+  puts "Options:"
+  puts
+  puts "  -f k       "
+  puts "  -f b6      "
+  puts "  -f kyodai  Kyōto Daigaku B6 index card"
+  puts "  -f c       "
+  puts "  -f 5x3     "
+  puts "  -f index   Index Card 3x5 inches (default)"
+  puts
+  exit 0
+end
+
+
 require 'hexapdf'
+require 'ostruct'
+
+KYODAI = OpenStruct.new(
+  name: 'kyodai', width: 85, font: 'Courier', fsize: 10, lheight: 11,
+  height: 33)
+INDEX = OpenStruct.new(
+  name: 'index', width: 65, font: 'Courier', fsize: 9, lheight: 9,
+  height: 23)
+
+FORMATS = {}
+FORMATS['kyodai'] = KYODAI
+FORMATS['b6'] = KYODAI
+FORMATS['k'] = KYODAI
+FORMATS['index'] = INDEX
+FORMATS['5x3'] = INDEX
+FORMATS['3x5'] = INDEX
+FORMATS['c'] = INDEX
+
+format = FORMATS[args['format'] || args['f'] || 'index']
+out = args['out'] || args['o'] || 'out.pdf'
+dir = args['dir'] || '.'
+
+out = File.join(dir, out) if ! out.match(/^\//)
+
+#p format
+#p args
+#p out
 
 doc = HexaPDF::Document.new
 page = doc.pages.add
 c = page.canvas
 
-c.font('Helvetica', size: 12).fill_color(0, 0, 0)
+X0 = 10
+Y0 = 800
 
-c.text('Hello World', at: [ 10, 10 ])
+c.font(format.font, size: format.fsize).fill_color(0, 0, 0)
 
-doc.write('out.pdf')
+if args['ruler']
+
+  s = ([ '0123456789' ] * 10).join('')
+  c.text(s[0, format.width], at: [ X0, Y0 ])
+  (1..format.height).each do |y|
+    s = y.to_s
+    s = s + ' ' + format.to_h.inspect if y == 1
+    c.text(s, at: [ X0, Y0 - y * format.lheight ])
+  end
+
+  doc.write(out)
+
+  exit 0
+end
+
+doc.write(out)
 
--- tmp/rulers.rb
+++ tmp/rulers.rb
@@ -3,11 +3,20 @@ require 'hexapdf'
 
 doc = HexaPDF::Document.new
 page = doc.pages.add
+#page = doc.pages.add(HexaPDF::Type::Page.media_box(:B6))
 c = page.canvas
 
-c.font('Helvetica', size: 12).fill_color(0, 0, 0)
+c.font('Courier', size: 12).fill_color(0, 0, 0)
 
-c.text('Hello World', at: [ 10, 10 ])
+R =
+  (0..9).collect { |k| "#{k}_23456789" }.join('') +
+  (0..9).collect { |k| "#{k}_23456789" }.join('')
 
-doc.write('out.pdf')
+(8..20).each do |size|
+  c.font_size = size
+  c.text("size: #{size}", at: [ 10, 10 + (size - 8) * 40 + 20 ])
+  c.text(R, at: [ 10, 10 + (size - 8) * 40 ])
+end
+
+doc.write('ruler.pdf')
 
